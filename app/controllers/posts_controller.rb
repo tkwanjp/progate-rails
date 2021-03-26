023dbdcc67625bc7d9039f579a2d7a97281a10e5
tkwanjp <tkwanjp@gmail.com>
2021-03-26 07:47:07 +0000

    added edit/update action.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,36 @@ class PostsController < ApplicationController
       @posts = Post.all
     end
 
+		def show
+			@post = Post.find_by id: params[:id]
+		end
 
+		def new
+			@post = Post.new
+		end
+
+		def create
+			@post = Post.new content: params[:content]
+			if @post.save
+				flash[:notice] = 'Successfully tweeted!'
+				redirect_to '/posts/index'
+			else
+				render 'posts/new'
+			end
+		end
+
+		def edit
+			@post = Post.find_by id: params[:id]
+		end
+		
+		def update
+			@post = Post.find_by id: params[:id]
+			@post.content = params[:content]
+			if @post.save
+				flash[:notice] = 'Successfully edited!'
+				redirect_to '/posts/index'
+			else
+				render 'posts/edit'
+			end
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'posts/index' => 'posts#index'
+	get 'posts/new' => 'posts#new'
+	get 'posts/:id' => 'posts#show'
+	get 'posts/:id/edit' => 'posts#edit'
+	post 'posts/create' => 'posts#create'
+	post 'posts/:id/update' => 'posts#update'
 
   get '/' => 'home#top'
   get '/about' => 'home#about'
